acb033ed188a44ecacc5ca9f7ec66cf14c7ec7b8
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-05-10 09:05:38 -0300

    5rhtf

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function() {
+
+alert(123);
+
+/* tipos de dados */
+/* String: Sequencia de caracter */
+/* Number: nú,meros inteiros ou pontos flutuantes (10.4) */
+/* Boolean:Retorna dois valores, true ou false */
+//object:estrutura de dados que pode armazenar multiplos valores
+//NULL:quando está vazio;
+//array:objeto especial ordenado 
+//function: blopco de código reutilizável;
+  })
