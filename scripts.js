c56f7aa410b394850db81f6fe007d4e1afab9cf0
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-05-10 09:49:40 -0300

    Uni

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded',function() {
 
-alert(123);
+//alert(123);
 
-/* tipos de dados */
-/* String: Sequencia de caracter */
-/* Number: nú,meros inteiros ou pontos flutuantes (10.4) */
-/* Boolean:Retorna dois valores, true ou false */
+//tipos de dados */
+//String: Sequencia de caracter */
+//Number: nú,meros inteiros ou pontos flutuantes (10.4) */
+//Boolean:Retorna dois valores, true ou false */
 //object:estrutura de dados que pode armazenar multiplos valores
 //NULL:quando está vazio;
 //array:objeto especial ordenado 
 //function: blopco de código reutilizável;
-  })
+  
+ const teste = 50; //const é usado para declarar uma variável cujo o valor é permanente 
+
+ //let é usado para declarar  variáveis que os valores podem ser retribuidos//
+
+ let C = 0 
+
+ console. log(C)
+
+ const notaMedia = 3;
+
+ if(notaMedia >= 6){
+  alert("Aluno passou");
+ }
+ else if (notaMedia >= 4 && notaMedia < 6) {
+alert("aluno quase passou")
+ }
+ else {
+  alert("aluno reprovou")
+ }
+});
