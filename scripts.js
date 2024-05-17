ab0f0af8c773504b2ffb9e541f4713f38b1afe76
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-05-17 10:31:48 -0300

    projeto

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.body.addEventListener('click', function(){
-        alert('123')
+    const baseImagens = {
+        todas: ['imagem.jpg','cidade.jpg','animal.jpg','natureza.jpg'],
+        natureza: [],
+        cidade: [],
+        natureza: [],
+
+    }
+    document.body.addEventListener('click', function(event){ //evento clique    '
+    if(event.target.classList.contains('botao-categoria'));  //acessa a lista de classe de onde clicou e //procura pela classe botao-categoria
+    alert(123);
+
     })
 })
