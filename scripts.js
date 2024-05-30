a43754faa69f106cb0541699751cda6cb91aa762
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-05-30 08:39:28 -0300

    trabalho

--- scripts.js
+++ scripts.js
@@ -7,26 +7,37 @@ document.addEventListener('DOMContentLoaded', function(){
         animal: ['animal.png'],
     }
     function carregaImagens(categoria){
-        //Selecionar elementos HTML com base em um seletor
-        const galeria = document.querySelector("#galeria-imagens");
-        galeria.innerHTML=''; //limpar bloco, apaga tudo dentro dele
-
+    //Selecionar elementos HTML com base em um seletor
+    const galeria = document.querySelector("#galeria-imagens");
+    galeria.innerHTML=''; //limpar bloco, apaga tudo dentro dele
+    
         const imagens = baseImagens[categoria]
         
         imagens.forEach(img => { 
             console.log(img);
-            galeria.innerHTML += '<div> <img src="imagens/' + img + '"/> </div>';
+            galeria.innerHTML += '<div class="imagem-item"> <img src="imagens/' + img + '"/> </div>';
         })
+    }
+    function ordenaImagens(ordem){
+    const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+    imagens.sort((a, b) => {
+console.log(a);
+    })
     }
     carregaImagens('todas');
     document.body.addEventListener('click', function(event){ //evento clique    '
-    if(event.target.classList.contains('botao-categoria')) {//acessa a lista de classe de onde clicou e //procura pela classe botao-categoria
-
-        const categoria = event.target.dataset.categoria;
-        
-        //alert(categoria);
-        carregaImagens(categoria);
-    }; 
+        if(event.target.classList.contains('botao-categoria')) {//acessa a lista de classe de onde clicou e //procura pela classe botao-categoria
+            
+            const categoria = event.target.dataset.categoria;
+            
+            //alert(categoria);
+            carregaImagens(categoria);
+        }; 
+        if(event.target.classList.contains('botao-ordenar')) {
+            const ordem = event.target.dataset.ordem;
+            ordenaImagens(ordem);
+            
+        }
 
     });
 });
